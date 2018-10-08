8d68dcc4a7ceabb6321ca54a2182e5086e821761
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-08 21:31:53 +0900

    Brew formula update for tbf version v0.1.0

--- tbf.rb
+++ tbf.rb
@@ -1,9 +1,9 @@
 class Tbf < Formula
   desc "CLI for tech book festival"
   homepage "https://github.com/mpppk/tbf"
-  url "https://github.com/mpppk/tbf/releases/download/v0.0.3/tbf_0.0.3_Darwin_x86_64.tar.gz"
-  version "0.0.3"
-  sha256 "f65093667362a35129b7d0462875fe8c5c42dc4fc35633bcd17fd4ff9c1ba8fe"
+  url "https://github.com/mpppk/tbf/releases/download/v0.1.0/tbf_0.1.0_Darwin_x86_64.tar.gz"
+  version "0.1.0"
+  sha256 "085fe5ea08c906c6f87f701002e2430580c5c9a4417db5bb4fcc1cb6527df9b1"
 
   def install
     bin.install "tbf"
